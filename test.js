2380f06d5a2316ee8ee3d8fa584fba23394da8d4
Vamsi Dharmavarapu <vamsi.akirah@gmail.com>
2017-12-26 21:51:45 +0530

    debug

--- test.js
+++ test.js
@@ -1,6 +1,124 @@
+const request = require('superagent');
+const exec = require('child_process').exec;
+const moment = require('moment');
 const USER = process.env.JIRA_USER;
 const PASSWORD = process.env.JIRA_PASSWORD;
 
-console.log('::::::::::::::::::::::::::USER::::::::::::::::::::::::::::::');
-console.log('USER' + USER);
-console.log('::::::::::::::::::::::::::USER::::::::::::::::::::::::::::::');
+const packageJson = require(__dirname + '/package.json');
+var versionId = packageJson.version; // old version
+
+// 1 - Bump the version
+// 3 - Get issues resolved since last version
+// 4 - Update issues with the new vesion id
+versionPatch()
+    .then((version) => createJiraVersion(version))
+    .then((version) => {
+        versionId = version; // new version
+        return getVersionIssues(packageJson.version);
+    })
+    .then((issues) => Promise.all(issues.map(issue => updateIssueWithVersion(issue, versionId))))
+    .then(() => console.log('Issues updated'))
+    .catch((e) => {
+        //console.log('Unable to update!');
+        console.log('USER :::::: ' + USER);
+        console.error(e);
+        process.exit(1);
+    });
+
+/**
+ * Bump the version
+ */
+function versionPatch() {
+    // execute npm version patch
+    return new Promise((resolve, reject) => {
+        exec(`npm version patch`, (error, stdout, stderr) => {
+            if (error) {
+                return reject(error);
+            } else {
+                return resolve(stdout.replace('v', '').replace(/\n/g, ''))
+            }
+        });
+    });
+}
+
+/**
+ * Create new version in Jira
+ */
+function createJiraVersion(version) {
+    console.log('new version : ' + version);
+    console.log('USER : ' + USER);
+    return new Promise((resolve, reject) => {
+        request.post(`https://backlog.acquia.com/rest/api/2/version`)
+            .auth(USER, PASSWORD)
+            .send({
+                description: "pipelines",
+                name: `pipelines-ui-${version}`,
+                userReleaseDate: moment().format('DD/MMM/YYYY'),
+                project: "MS",
+                archived: false,
+                released: false
+            })
+            .end((err, res) => {
+                if (err) {
+                    return reject(err);
+                } else {
+                    return resolve(version);
+                }
+            })
+    })
+}
+
+/**
+ * Get all issue number resolved since given version
+ */
+function getVersionIssues(versionId) {
+    return new Promise((resolve, reject) => {
+        exec('git --no-pager log -100', (error, stdout, stderr) => {
+            if (error) {
+                return reject(error);
+            } else {
+                const search = stdout.match(versionId);
+                const tmp = stdout.substring(0, search.index).match(/MS-\d{4,}/g) || [];
+
+                let issues = [];
+                if (tmp != null) {
+                    for (var i = 0; i < tmp.length; i++) {
+                        if (issues.indexOf(tmp[i]) == -1) {
+                            issues.push(tmp[i]);
+                        }
+                    }
+                }
+                console.log(issues);
+                return resolve(issues);
+            }
+        });
+    });
+}
+
+/**
+ * Update the JIRA issue with the given version
+ */
+function updateIssueWithVersion(issueId, version) {
+    console.log('updating the issue id:', issueId);
+    return new Promise((resolve, reject) => {
+        let url = `https://backlog.acquia.com/rest/api/2/issue/` + issueId;
+        request.put(url)
+            .auth(USER, PASSWORD)
+            .send({
+                update: {
+                    fixVersions: [{
+                        set: [{
+                            name: `pipelines-ui-${version}`
+                        }]
+                    }]
+                }
+            })
+            .end((err, res) => {
+                if (err) {
+                    return reject(err);
+                } else {
+                    return resolve();
+                }
+            })
+    })
+}
