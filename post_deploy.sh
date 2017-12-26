24dac9f436c641a2828fe4dadf4054392e4fbe6d
Vamsi Dharmavarapu <vamsi.akirah@gmail.com>
2017-12-26 21:38:18 +0530

    debug

---
+++ post_deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+
+node $SOURCE_DIR/test.js
---
+++ test.js
@@ -0,0 +1,6 @@
+const USER = process.env.JIRA_USER;
+const PASSWORD = process.env.JIRA_PASSWORD;
+
+console.log('::::::::::::::::::::::::::USER::::::::::::::::::::::::::::::');
+console.log('USER' + USER);
+console.log('::::::::::::::::::::::::::USER::::::::::::::::::::::::::::::');
