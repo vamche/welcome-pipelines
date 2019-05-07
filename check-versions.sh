df33d60918dc3b241e00e534f3c66e1a9687a221
Vamsi Dharmavarapu <vamsi.akirah@gmail.com>
2019-05-07 23:00:16 +0530

    Update check-versions.sh

--- check-versions.sh
+++ check-versions.sh
@@ -2,18 +2,24 @@
 
 # The arguments are tool name, version string, and expected version.
 echo "Checking ${1} version..."
-if [ "$5" != "" ]; then
-  COMMAND=$(echo $($2) | grep "$3" $4 | grep "$5")
+if [ "$4" != "" ]; then
+  echo "$2 | grep '$3' -2 | grep '$4'"
+  COMM=`$2 | grep "$3" -2 | grep "$4"`
 else
-  COMMAND=$(echo $($2) | grep "$3")
+  echo "$2 | grep '$3'"
+  COMM=`$2 | grep "$3"`
 fi
 if [ $? -ne 0 ]; then
   MESSAGE="Version check failed for $1. Expected $3, got $($2)."
   echo $MESSAGE >> /tmp/version_check_failure
-  echo "[FAIL] $($2 | head -n1)"
+  if [ "$4" != "" ]; then
+    echo -e "[FAIL]" `$2 | grep "$3" -2`
+  else
+    echo -e "[FAIL]" `$2`
+  fi
 else
   MESSAGE="Version check completed successfully for $1: $3."
   echo $MESSAGE >> /tmp/version_check_success
-  echo "[SUCCESS] $($2 | head -n1)"
+  echo "[SUCCESS]"
 fi
 echo ""
