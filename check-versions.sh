26a40bd18fff0d53a2a4a03116ec55f49a545f1b
Vamsi Dharmavarapu <vamsi.akirah@gmail.com>
2019-05-07 20:23:40 +0530

    Update check-versions.sh

--- check-versions.sh
+++ check-versions.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
 # The arguments are tool name, version string, and expected version.
-echo -n "Checking ${1} version..."
-echo -e $2 | grep $3 > /dev/null 2>&1
-exit_code=$?
-
-if [[ $exit_code -ne 0 ]]
-then
-  MESSAGE="Version check failed for ${1}. Expected ${3}, got ${2}."
-  echo $MESSAGE >> /tmp/version_check_failure
-  echo "FAIL - Version: ${2}."
+echo "Checking ${1} version..."
+if [ "$5" != "" ]; then
+  COMMAND=$(echo $($2) | grep "$3" $4 | grep "$5")
 else
-  MESSAGE="Version check completed successfully for ${1}. Version: ${3}."
-  echo $MESSAGE >> /tmp/version_check_success
-  echo "SUCCESS - Version: ${3}."
+  COMMAND=$(echo $($2) | grep "$3")
 fi
+if [ $? -ne 0 ]; then
+  MESSAGE="Version check failed for $1. Expected $3, got $($2)."
+  echo $MESSAGE >> /tmp/version_check_failure
+  echo "[FAIL] $($2 | head -n1)"
+else
+  MESSAGE="Version check completed successfully for $1: $3."
+  echo $MESSAGE >> /tmp/version_check_success
+  echo "[SUCCESS] $($2 | head -n1)"
+fi
+echo ""
