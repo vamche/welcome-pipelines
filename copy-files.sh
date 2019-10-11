9f1592b15eda911d15d0c5e88975b1bd242b1126
Vamsi Dharmavarapu <vamsi.dharmavarapu@acquia.com>
2019-10-11 08:27:58 +0530

    Update copy-files.sh

--- copy-files.sh
+++ copy-files.sh
@@ -4,9 +4,10 @@ set -e
 # Get Authentication token
 TOKEN=$(curl -sS -X POST -u "${CLOUD_API_KEY}:${CLOUD_API_SECRET}" -d "grant_type=client_credentials" https://accounts.acquia.com/api/auth/oauth/token | python -c "import sys, json; print json.load(sys.stdin)['access_token']")
 
-cat $PIPELINES_SCRIPT_DATA_FILE
+# Get CDE Name
+CDE_NAME = $(python -c "import sys, json; envs=json.load('$PIPELINES_SCRIPT_DATA_FILE')['environment']['PIPELINES_DEPLOYMENT_NAME']")
 
 # Get CDE id
-ENV_ID=$(curl -sS -X GET "https://cloud.acquia.com/api/applications/$PIPELINE_APPLICATION_ID/environments" -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}" | python -c "import sys, json; envs=json.load(sys.stdin)['_embedded']['items']; print [x for x in envs if x['label'] == '$PIPELINE_DEPLOY_VCS_PATH'][0]['id']")
+ENV_ID=$(curl -sS -X GET "https://cloud.acquia.com/api/applications/$PIPELINE_APPLICATION_ID/environments" -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}" | python -c "import sys, json; envs=json.load(sys.stdin)['_embedded']['items']; print [x for x in envs if x['label'] == '$CDE_NAME'][0]['id']")
 
 echo $ENV_ID
