453a57ed66b974ab6fef5bb7a43e62835e7b366a
Vamsi Dharmavarapu <vamsi.akirah@gmail.com>
2019-10-11 09:01:23 +0530

    updated copy-files

--- copy-files.sh
+++ copy-files.sh
@@ -11,8 +11,8 @@ CDE_NAME=$(cat $PIPELINES_SCRIPT_DATA_FILE | python -c "import sys, json; print 
 ENV_ID=$(curl -sS -X GET "https://cloud.acquia.com/api/applications/$PIPELINE_APPLICATION_ID/environments" -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}" | python -c "import sys, json; envs=json.load(sys.stdin)['_embedded']['items']; print [x for x in envs if x['name'] == '$CDE_NAME'][0]['id']")
 
 # Get Stage id
-STAGE_ENV_ID=$(curl -sS -X GET "https://cloud.acquia.com/api/applications/$PIPELINE_APPLICATION_ID/environments" -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}" | python -c "import sys, json; envs=json.load(sys.stdin)['_embedded']['items']; print [x for x in envs if x['name'] == 'stage'][0]['id']")
-
+STAGE_ENV_ID=$(curl -sS -X GET "https://cloud.acquia.com/api/applications/$PIPELINE_APPLICATION_ID/environments" -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}" | python -c "import sys, json; envs=json.load(sys.stdin)['_embedded']['items']; print [x for x in envs if x['name'] == 'test'][0]['id']")
+# STAGE_ENV_ID=31098-d6a43c82-cc6e-4426-b6eb-883cbe4a99ea
 echo $STAGE_ENV_ID
 
 # Copy files
