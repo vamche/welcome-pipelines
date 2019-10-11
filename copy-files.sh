6f7f541bf49c35bd2b3ff8de2ce1423b9e1025c7
Vamsi Dharmavarapu <vamsi.akirah@gmail.com>
2019-10-11 08:47:56 +0530

    updated copy-files

--- copy-files.sh
+++ copy-files.sh
@@ -7,6 +7,9 @@ TOKEN=$(curl -sS -X POST -u "${CLOUD_API_KEY}:${CLOUD_API_SECRET}" -d "grant_typ
 # Get CDE Name
 CDE_NAME=$(cat $PIPELINES_SCRIPT_DATA_FILE | python -c "import sys, json; print json.load(sys.stdin)['environment']['PIPELINES_DEPLOYMENT_NAME']")
 
+cat $PIPELINES_SCRIPT_DATA_FILE
+echo $CDE_NAME
+
 # Get CDE id
 ENV_ID=$(curl -sS -X GET "https://cloud.acquia.com/api/applications/$PIPELINE_APPLICATION_ID/environments" -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}" | python -c "import sys, json; envs=json.load(sys.stdin)['_embedded']['items']; print [x for x in envs if x['label'] == '$CDE_NAME'][0]['id']")
 
