fb2e8ba125635c9b38f2a24be1c3dff99883d209
Vamsi Dharmavarapu <vamsi.akirah@gmail.com>
2019-10-11 08:58:07 +0530

    updated copy-files

--- copy-files.sh
+++ copy-files.sh
@@ -4,14 +4,16 @@ set -e
 # Get Authentication token
 TOKEN=$(curl -sS -X POST -u "${CLOUD_API_KEY}:${CLOUD_API_SECRET}" -d "grant_type=client_credentials" https://accounts.acquia.com/api/auth/oauth/token | python -c "import sys, json; print json.load(sys.stdin)['access_token']")
 
-cat $PIPELINES_SCRIPT_DATA_FILE
-
 # Get CDE Name
 CDE_NAME=$(cat $PIPELINES_SCRIPT_DATA_FILE | python -c "import sys, json; print json.load(sys.stdin)['environment']['PIPELINES_DEPLOYMENT_NAME']")
-
-echo $CDE_NAME
 
 # Get CDE id
 ENV_ID=$(curl -sS -X GET "https://cloud.acquia.com/api/applications/$PIPELINE_APPLICATION_ID/environments" -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}" | python -c "import sys, json; envs=json.load(sys.stdin)['_embedded']['items']; print [x for x in envs if x['name'] == '$CDE_NAME'][0]['id']")
 
-echo $ENV_ID
+# Get Stage id
+STAGE_ENV_ID=$(curl -sS -X GET "https://cloud.acquia.com/api/applications/$PIPELINE_APPLICATION_ID/environments" -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}" | python -c "import sys, json; envs=json.load(sys.stdin)['_embedded']['items']; print [x for x in envs if x['name'] == 'stage'][0]['id']")
+
+echo $STAGE_ENV_ID
+
+# Copy files
+curl -X POST "https://cloud.acquia.com/api/environments/$ENV_ID/files" -d "{\"source\":$STAGE_ENV_ID}" -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}"
