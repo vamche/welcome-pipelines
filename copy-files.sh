9f9311beee51d883898430932d4f636be678aee4
Vamsi Dharmavarapu <vamsi.akirah@gmail.com>
2019-10-11 08:50:32 +0530

    updated copy-files

--- copy-files.sh
+++ copy-files.sh
@@ -4,10 +4,11 @@ set -e
 # Get Authentication token
 TOKEN=$(curl -sS -X POST -u "${CLOUD_API_KEY}:${CLOUD_API_SECRET}" -d "grant_type=client_credentials" https://accounts.acquia.com/api/auth/oauth/token | python -c "import sys, json; print json.load(sys.stdin)['access_token']")
 
+cat $PIPELINES_SCRIPT_DATA_FILE
+
 # Get CDE Name
 CDE_NAME=$(cat $PIPELINES_SCRIPT_DATA_FILE | python -c "import sys, json; print json.load(sys.stdin)['environment']['PIPELINES_DEPLOYMENT_NAME']")
 
-cat $PIPELINES_SCRIPT_DATA_FILE
 echo $CDE_NAME
 
 # Get CDE id
